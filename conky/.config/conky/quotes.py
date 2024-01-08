6e3ff784be3cf3db9c16d7879dbb724a01b20fb0
John Knowles <jtknowles007@gmail.com>
2024-01-07 22:32:21 -0500

    Initial commit

---
+++ conky/.config/conky/quotes.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+import getjson
+import textwrap
+url = 'https://api.quotable.io/quotes/random'
+
+feed = getjson.getjson(url)
+quote = feed[0]['content']
+author = feed[0]['author']
+quotewrap = textwrap.fill(quote,45)
+print(quotewrap)
+print("- " + author)
