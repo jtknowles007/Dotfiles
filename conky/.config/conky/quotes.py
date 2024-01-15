3154326bbf11a23b5683c1f277a2b078a2619d99
John Knowles <jtknowles007@gmail.com>
2024-01-15 09:24:44 -0500

    Revised structure of output.

--- conky/.config/conky/quotes.py
+++ conky/.config/conky/quotes.py
@@ -3,12 +3,14 @@
 import json
 import textwrap
 from random import randint
-with open('moviequotes.json','r') as fcc_file:
+with open('quotes.json','r') as fcc_file:
     fcc_data=json.load(fcc_file)
     res = sum(1 for i in fcc_data if type(i)==dict)
     value=randint(0,res)
 movie = fcc_data[value]['quote']
 source = fcc_data[value]['source']
 quotewrap = textwrap.fill(movie,35)
+sourcewrap = textwrap.fill(source,35)
 print(quotewrap)
-print("- " + source)
+print("")
+print(sourcewrap)
