125fa67bb23e8777031b0bd3de8bb8fba614b97e
John Knowles <jtknowles007@gmail.com>
2024-01-26 22:22:03 -0500

    Revised line gathering and output.  Comments.

--- conky/.config/conky/alert.py
+++ conky/.config/conky/alert.py
@@ -1,18 +1,32 @@
 #! /usr/bin/env python3
 import textwrap
 
+# Open alert.txt file and read into variable
 myalert = open('alert.txt','r')
 alert = myalert.read()
 myalert.close
-mylines = alert.splitlines()
-title = textwrap.fill(mylines[1],width=35)
-fullist = []
-fullist.append(mylines[5])
-fullist.append(mylines[14])
-fullist.append(mylines[17])
-merged_fullist = " ".join(str(element) for element in fullist)
-wrapped_text = textwrap.fill(merged_fullist,width=35)
 
-print("${font IBM Plex Mono:size=12}")
+# Split lines and determine position of items of interest within the list variable
+mylines = alert.splitlines()
+what = mylines.index('WHAT')+1
+impact = mylines.index('IMPACTS')+1
+
+# Assign items of interest to variables
+whattext = mylines[what]
+impacttext = mylines[impact]
+
+# Assign alert title to variable
+title = textwrap.fill(mylines[1],width=35)
+
+# Join items of interest into one text block and wrap text to fit conky window 
+fullist = []
+fullist.append(whattext)
+fullist.append(impacttext)
+
+mergedtext = " ".join(str(element) for element in fullist)
+wrappedtext = textwrap.fill(mergedtext,width=35)
+
+# Output to Conky
+print(mylines[0]+"\n")
 print(title + "\n")
-print(wrapped_text)
+print(wrappedtext)
