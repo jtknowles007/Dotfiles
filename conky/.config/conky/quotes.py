84203c5d3f799b50183a682b0cf8be07e732bc15
John Knowles <jtknowles007@gmail.com>
2024-02-03 09:34:27 -0500

    File Linted

--- conky/.config/conky/quotes.py
+++ conky/.config/conky/quotes.py
@@ -9,18 +9,17 @@ quotevar = quoteslist.thevar
 
 # Iterate over dicts within the list; find how many are there,
 # and generate a random number within the count of available quotes
-res = sum(1 for i in quotevar if type(i)==dict)
-value=randint(0,res)
+res = sum(1 for i in quotevar if type(i) == dict)
+value=randint(0, res)
 
 # Variables
 movie = quotevar[value]['quote']
 source = quotevar[value]['source']
 speaker = quotevar[value]['speaker']
-#speaker = quotevar[value]['speaker']
-quotewrap = textwrap.fill(movie,40)
-sourcewrap = textwrap.fill(source,40)
-speakerwrap = textwrap.fill(speaker,40)
+quotewrap = textwrap.fill(movie, 40)
+sourcewrap = textwrap.fill(source, 40)
+speakerwrap = textwrap.fill(speaker, 40)
 
 # Output
-print("${font IBM Plex Mono:size=10}" + quotewrap+"\n\n"+speakerwrap+"\n"+sourcewrap)
-#print("${font IBM Plex Mono:size=10}" + quotewrap+"\n\n"+sourcewrap)
+print("${font IBM Plex Mono:size=10}" + quotewrap + "\n\n" +
+      speakerwrap + "\n" + sourcewrap)
