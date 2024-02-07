5060b9dc879d07e1624f1cec01b86bf93da2aa9b
John Knowles <jtknowles007@gmail.com>
2024-02-06 21:51:37 -0500

    Linted variables

--- conky/.config/conky/moonphase.py
+++ conky/.config/conky/moonphase.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python3
+""" Get current phase of moon and output corresponding image to Conky """
+
 import requests
 from bs4 import BeautifulSoup
-moonphaseURL = 'https://www.moongiant.com/phase/today'
-moonimages = '/home/john/.config/conky/moonicons/'
-moonpage = requests.get(moonphaseURL)
-moonsoup = BeautifulSoup(moonpage.content, "html.parser")
-moontoday = moonsoup.find(id='today_')
-moonsrc = moontoday.find_all("img")
-for image in moonsrc:
-    thevar = image['src']
-thevar = moonimages + thevar[20:-4] + '.png'
-print("${{image {} -p 225,450 -s 60x60}}".format(thevar))
+
+MOON_PHASE_URL = 'https://www.moongiant.com/phase/today'
+MOON_IMAGES = '/home/john/.config/conky/moonicons/'
+MOON_PAGE = requests.get(MOON_PHASE_URL, timeout=15)
+MOON_SOUP = BeautifulSoup(MOON_PAGE.content, "html.parser")
+MOON_TODAY = MOON_SOUP.find(id='today_')
+MOON_SOURCE = MOON_TODAY.find_all("img")
+for MOON_IMG in MOON_SOURCE:
+    MOON_PHASE = MOON_IMG['src']
+MOON_PHASE = MOON_IMAGES + MOON_PHASE[20:-4] + '.png'
+print("${{image {} -p 225,450 -s 60x60}}".format(MOON_PHASE))
