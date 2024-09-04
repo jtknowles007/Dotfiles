485d455c0391b573eabf49fd90027571221e5924
John Knowles <jtknowles007@gmail.com>
2024-09-04 10:54:36 -0400

    Remove upgrade logic

--- polybar/.config/polybar/scripts/upgradable.sh
+++ polybar/.config/polybar/scripts/upgradable.sh
@@ -6,13 +6,6 @@ if [ "$ST" = "$NU" ]; then
     ARR="Up to date"
 else
     ARR=($ST)
-    ARR+=("UPGRADE")
 fi
 
 CHOICE=$(printf '%s\n' "${ARR[@]}" | rofi -dmenu -theme ~/.config/polybar/scripts/rofi/upd.rasi -p "")
-
-if [ "$CHOICE" = "UPGRADE" ]; then
-    upgrade
-    exit 0
-fi
-
