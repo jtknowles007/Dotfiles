6931b048f9a2d33ff9c8f1111d95823938f5804c
John Knowles <jtknowles007@gmail.com>
2024-10-14 12:17:37 -0400

    PwrSv indicator color change

--- polybar/.config/polybar/scripts/dpms.sh
+++ polybar/.config/polybar/scripts/dpms.sh
@@ -3,7 +3,7 @@
 DPMS_STATUS=$(xset q | grep "DPMS is" | awk '{print $3}')
 
 if [ "$DPMS_STATUS" == "Enabled" ]; then
-    echo " %{F#66ff66}󰌪%{F-} "
+    echo " %{F#F37413}󰌪%{F-} "
 else
     echo " %{F#CACACA}󰌪%{F-} "
 fi
