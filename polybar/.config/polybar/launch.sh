ba865d92efc4130907037d553bc10ca02a1ddd6e
John Knowles <jtknowles007@gmail.com>
2024-10-14 10:45:06 -0400

    Display bar/three

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,3 +7,4 @@ polybar-msg cmd quit
 echo "---" | tee -a /tmp/polybar1.log tmp/polybar2.log
 polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar secondary 2>&1 | tee /tmp/polybar2.log & disown
+polybar three 2>&1 | tee /tmp/polybar3.log & disown
